bd4704689e71378160c2c7e2f8ed70beab651e28
Nigel <nrstirzaker.techblog@gmail.com>
2021-02-26 20:44:48 +0000

    added "GET" health check back in

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express()
 app.use(express.json());
 const port = 3000
 
+app.get('/', function (req, res) {
+    res.send('Alive:' + new Date())
+})
 
 app.listen(port, function () {
     //note the quote marks are actually backsticks
